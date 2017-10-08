8ff6d8a4644869fc53938fe986262256bd40098d
redfoo <redfoo@redfoos-MacBook-Pro.local>
2017-10-07 22:48:50 -0700

    working

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,25 +1,14 @@
 import App
-
-/// We have isolated all of our App's logic into
-/// the App module because it makes our app
-/// more testable.
-///
-/// In general, the executable portion of our App
-/// shouldn't include much more code than is presented
-/// here.
-///
-/// We simply initialize our Droplet, optionally
-/// passing in values if necessary
-/// Then, we pass it to our App's setup function
-/// this should setup all the routes and special
-/// features of our app
-///
-/// .run() runs the Droplet's commands,
-/// if no command is given, it will default to "serve"
+import MySQLProvider
 let config = try Config()
+try config.addProvider(MySQLProvider.Provider.self)
 try config.setup()
-
 let drop = try Droplet(config)
 try drop.setup()
-
+drop.get("version") {
+    request in let db = try drop.mysql()
+    let version = try
+        db.raw("SELECT version()")
+    return JSON(node: version)
+}
 try drop.run()
